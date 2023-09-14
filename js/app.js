d7f92dcd14911aa9620e2b3848b74d202b722fbc
devomohamed <mohammed.sabry633@gmail.com>
2023-09-14 22:54:16 +0300

    New Version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-let myArr = ["Mohamed" , "Sabry", "Abdelmordy"]
-let typeWriter = new TypeWriter(".type-writer",myArr);
-typeWriter.render()
+var app = document.querySelector('.type-writer');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Frontend Developer (Angular)')
+    .pauseFor(2500)
+    .deleteAll()
+    // .typeString('Backend Developer (Dotnet)')
+    // .pauseFor(2500)
+    // .deleteAll()
+    // .typeString('Fullstack (Angular, Dotnet)')
+    // .pauseFor(2500)
+    .start();
