cc0a0e8f73882e302224bd365f62ce2a44aeed1f
devomohamed <mohammed.sabry633@gmail.com>
2023-09-20 00:59:00 +0300

    New Version 00

--- js/index.js
+++ js/index.js
@@ -91,13 +91,13 @@ let projects = {
   "angular":[
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -105,13 +105,13 @@ let projects = {
     },
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -119,13 +119,13 @@ let projects = {
     },
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -135,13 +135,13 @@ let projects = {
   "vanilla_javascript":[
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -149,13 +149,13 @@ let projects = {
     },
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -163,13 +163,13 @@ let projects = {
     },
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -179,13 +179,13 @@ let projects = {
   "companies_tasks":[
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -193,27 +193,13 @@ let projects = {
     },
     {
       name:"Trackify",
-      image:'',
+      image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
       startDate:"03-2022",
       endDate:"03-2022",
       description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
       authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
-      ],
-      githubRepo:"",
-      liveDemo:"",
-      tools:["Html","Css","Js","Jquery","Bootstrap"]
-    },
-    {
-      name:"Trackify",
-      image:'',
-      startDate:"03-2022",
-      endDate:"03-2022",
-      description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
-      authers:[
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"},
-        {image:'',name:"Mohamed",github:"https://github.com/devomohamed"}
+        {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""},
+        {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:""}
       ],
       githubRepo:"",
       liveDemo:"",
@@ -334,7 +320,7 @@ function getProjectById(id){
   if(id == "all") return getProjectsAll()
   let htmlText = ``;
   projects[id].forEach(element => {
-    let txtHtml = `<div class="project">
+    let txtHtml = `<div class="project" onclick="popup(${element})">
     <img src="${element.image}" alt="">
     <ul class="tools">`
     // console.log(element.tools);
@@ -425,14 +411,20 @@ var templateParams = {};
 
 
 function sendEmail(){
-  var templateParams = {
-    from_name: document.getElementById('name').value,
-    message:document.getElementById('message').value,
-    email_id:document.getElementById('email').value,
-    subject:document.getElementById('subject').value
-  };
+  let from_name = document.getElementById('name').value
+  let message = document.getElementById('message').value
+  let email_id = document.getElementById('email').value
+  subject = document.getElementById('subject').value
   
-  emailjs.send(serviceId, templateId, templateParams)
+  var templateParams = {
+    from_name,
+    message,
+    email_id,
+    subject
+  };
+
+  if (from_name !== "" || message != "" || email_id != "" || subject != "") {
+    emailjs.send(serviceId, templateId, templateParams)
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
        Swal.fire(
@@ -443,6 +435,9 @@ function sendEmail(){
     }, function(error) {
        console.log('FAILED...', error);
     });
+  }
+  
+  
 }
 
 document.querySelector('form').addEventListener('submit', function(e){
@@ -451,12 +446,84 @@ document.querySelector('form').addEventListener('submit', function(e){
 } )
 
 
-function Popup(){
+function popup(project){
   let overlay = document.createElement("div");
-  // overlay.className = "popup-overlay";
   overlay.classList.add("popup-overlay")
+
+  let txtHtml = `<div class="project">`
+  txtHtml += `
+    <div class="top">
+      <button class="close" onclick="closePopup()"><i class="fa-solid fa-xmark fa-lg"></i></button>
+    </div>
+    <img src="${project.image}" alt="">
+    <div class="project-name">${project.name}</div>
+    <div class="date"><span class="start-date">${representDate(project.startDate)}</span> - <span class="end-date">${representDate(project.endDate)}</span></div>
+    `
+
+    
+    txtHtml += `<ul class="tools">`
+
+    project.tools.forEach(tool => {
+      txtHtml +=`<li>${tool}</li>`
+    });
+
+    txtHtml +=`</ul>
+    <p class="description">${project.description}</p>`
+
+    txtHtml +=`<div class="authers">`
+
+    project.authers.forEach(author => {
+      txtHtml +=`
+      <div class"mauther">
+        <div class="left">
+          <img src="${author.image}" alt="">
+          <h3>${author.name}</h3>
+        </div>
+        <div class="right">
+          <a href="${author.github}"><i class="fa-brands fa-github fa-lg" style="color: #ffffff;"></i></a>
+          <a href="${author.linkedIn}"><i class="fa-brands fa-linkedin fa-lg" style="color: #ffffff;"></i></a>
+        </div>
+      </div>
+      `
+    });
+
+
+    txtHtml += `</div>`
+    txtHtml += `<div class="project-links">
+      <a href="${project.githubRepo}">View Code</a>
+      <a href="${project.liveDemo}">View Live Demo</a>
+    </div>`
+
+
+
+
+    txtHtml += `</div>
+    </div>`
+
+    overlay.innerHTML = txtHtml
   
-  document.body.appendChild(overlay);
-  
+  // document.body.appendChild(overlay);
+  document.body.append(overlay)
+
+  document.body.style.overflowY = 'hidden'
 }
-Popup()
+
+function closePopup(){
+  document.querySelector('.popup-overlay').remove()
+  document.body.style.overflowY = 'scroll'
+}
+
+popup({
+  name:"Trackify",
+  image:'../images/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
+  startDate:"03-2022",
+  endDate:"03-2022",
+  description:`Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. Admin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee`,
+  authers:[
+    {image:'../images/100614635.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:"#"},
+    {image:'../images/64485885.jpeg',name:"Mohamed",github:"https://github.com/devomohamed",linkedIn:"#"}
+  ],
+  githubRepo:"#",
+  liveDemo:"#",
+  tools:["Html","Css","Js","Jquery","Bootstrap"]
+})
